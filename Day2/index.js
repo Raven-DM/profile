ec059e89c29e2e3c29ace096f933022c27492d80
Raven DM <ravendaniel0@gmail.com>
2025-06-24 09:55:28 +0700

    challange 1

---
+++ Day2/index.js
@@ -0,0 +1,39 @@
+let pesan = 'Message to the world';
+// console.log(pesan);
+
+pesan = 'message changed';
+// console.log(pesan);
+
+const bumi = 'bulat';
+// bumi = 'datar';
+
+
+const arr = ['banana', true, 23]
+
+// console.log(arr[2] * 2);
+
+const person = [
+    {
+        name: 'John',
+        age: 30,
+        isStudent: false
+    },
+    {
+        name: 'Doe',
+        age: 25,
+        isStudent: false
+    }
+];
+
+// console.log(person[0].name);
+
+let x = 2;
+let y = '2';
+
+// console.log(x += y);
+console.log(x == y)
+
+let age = 10
+console.log(age >= 18 && age <= 55 ? 'Legal' : age > 56 ? 'Not Legal' : 'Undefined age');
+
+
